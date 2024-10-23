f9585e523379c9f634c5d9962edc8414bc6d67d9
Manish Khadka <mrsomeonne@gmail.com>
2024-10-23 17:11:47 +0545

    path variable

--- src/main/java/com/mrsomeonne/FirstSpring/HelloController.java
+++ src/main/java/com/mrsomeonne/FirstSpring/HelloController.java
@@ -1,16 +1,13 @@
 package com.mrsomeonne.FirstSpring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public HelloResponse hello(){
-        return new HelloResponse("Hello, World!");
+    @GetMapping("/hello/{name}")
+    public HelloResponse hello(@PathVariable String name){
+        return new HelloResponse("Hello, "+name+"!");
     }
 
     @PostMapping("/hello")
